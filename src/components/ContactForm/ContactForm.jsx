62736fe08ce380df6d1f215e92dbb9c554e4a550
Viktoriia Cherkashyna <vikushacherk98@gmail.com>
2023-10-05 14:31:01 +0300

    added placeholders

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -65,6 +65,7 @@ export default function Form({ onSubmit }) {
           name="name"
           value={name}
           onChange={handleInputChange}
+          placeholder="Adam Smith"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
@@ -77,6 +78,7 @@ export default function Form({ onSubmit }) {
           name="number"
           value={number}
           onChange={handleInputChange}
+          placeholder="+380(88)-233-22-22"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
